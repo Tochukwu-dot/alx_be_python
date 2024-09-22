08813490c9e63b56dfec3ae24e8963183e63dfb4
Tochukwu <nwibetochukwu@gmail.com>
2024-09-22 13:02:14 +0100

    Multiplication Table for 5

--- control-flow/multiplication_table.py
+++ control-flow/multiplication_table.py
@@ -1,5 +1,5 @@
 number = int(input("Enter a number to see its multiplication table: "))
-our_range = range(1, 11)
-for x in our_range:
+range = range(1, 11)
+for x in range:
     result = number * x
     print(number, "*", x, "=", result)
