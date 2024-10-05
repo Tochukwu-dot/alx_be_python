1723f6cb4e286491eae567c4e6e8e25ac537b1a1
Tochukwu <nwibetochukwu@gmail.com>
2024-10-05 21:40:06 +0100

    Changes

--- programming_paradigm/bank_account.py
+++ programming_paradigm/bank_account.py
@@ -9,7 +9,7 @@ class BankAccount:
         """Add the specified amount to the account balance."""
         if amount > 0:
             self.__account_balance += amount
-            print(f"Deposited: ${amount}")
+            print(f"Deposited: ${amount:.2f}")
         else:
             print("Deposit amount must be positive.")
     
