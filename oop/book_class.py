e99b77d65c8be18095d180cf078cc3fac9d95aa0
Tochukwu <nwibetochukwu@gmail.com>
2024-10-12 21:24:55 +0100

    Mastering OOP

--- oop/book_class.py
+++ oop/book_class.py
@@ -9,21 +9,21 @@ class Book:
     
     def __del__(self):
         """Destructor to handle object deletion"""
-        print(f"Deleting '{self.title}'")
+        print(f"Deleting {self.title}")
 
     def __str__(self):
         """String representation of a Book instance"""
-        return f"'{self.title}' by {self.author}, published in {self.year}"
+        return f"{self.title} by {self.author}, published in {self.year}"
     
     def __repr__(self):
         """Official representation to recreate the Book instance"""
-        return f"Book('{self.title}', '{self.author}', {self.year})"
+        return f"Book({self.title}, {self.author}, {self.year})"
 
 # Example usage:
-if __name__ == "__main__":
-    book1 = Book("1984", "George Orwell", 1949)
-    print(str(book1))   # Output: '1984' by George Orwell, published in 1949
-    print(repr(book1))  # Output: Book('1984', 'George Orwell', 1949)
+# if __name__ == "__main__":
+#     book1 = Book("1984", "George Orwell", 1949)
+#     print(str(book1))   # Output: '1984' by George Orwell, published in 1949
+#     print(repr(book1))  # Output: Book('1984', 'George Orwell', 1949)
     
-    # Deleting the object
-    del book1
+#     # Deleting the object
+#     del book1
